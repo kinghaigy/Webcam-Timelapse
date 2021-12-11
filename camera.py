190f69bf16d68f3452e9fe0c7aeb05ee4be7c55d
kinghaigy <kinghaig@gmail.com>
2021-12-11 14:33:02 +1030

    First working version of the new timelapse
    You can add multiple cameras with start and stop hours and it will run forever until stopped and log data. No work has been done in generating lists for ffmpeg yet.

--- camera.py
+++ camera.py
@@ -1,19 +1,25 @@
+from datetime import datetime
 import time
-from threading import *
+import os
+import logging
+import urllib.request
+import glob
+import re
+from exif import Image
 
-class Camera(Thread):
-    def __init__(self, name, URL, startTime, stopTime, interval):
-        
-        # Threading
-        Thread.__init__(self, target=self.startTimelapse)
-        self.daemon = True
 
+class Camera():
+    def __init__(self, name, URL, savePath, startTime, stopTime, interval):
+        logging.getLogger()
         # check types
         if not isinstance(name, str):
             raise Exception("name parameter must be a string")
         
         if not isinstance(URL, str):
             raise Exception("URL parameter must be a string")
+
+        if not isinstance(savePath, str):
+            raise Exception("savePath parameter must be a string")
         
         if not isinstance(interval, int):
             raise Exception("interval must be an integer > 0")
@@ -38,18 +44,75 @@ class Camera(Thread):
         self.startTime = startTime
         self.stopTime = stopTime
         self.interval = interval
+        self.savePath = os.path.normpath(savePath)
+        self.glob = None
+
+        # can we save to the save path right now?
+        if not os.path.isdir(self.savePath):
+            logging.info(self.savePath + " didn't exist. Making it now")
+            os.makedirs(self.savePath)
+
     
     def startTimelapse(self):
         self.running = True
         while self.running:
-            print("saving image from: " + self.name)
+            # Moved the sleep to the top to work with the 'continue' statement when an image doesn't get saved.
             time.sleep(self.interval)
+
+            # Are we inside working hours?
+            now = datetime.now()
+            if now.hour >= self.startTime and now.hour < self.stopTime:
+                print("Saving image from: " + self.name)
+                snap = None
+                try:
+                    snap = urllib.request.urlopen(self.URL)
+
+                    # is the image corrupted? (smaller than 90kb)
+                    if (snap.length < 90000):
+                        logging.error("Image appears to be corrupt: " + self.URL)
+                        continue
+                except Exception as e:
+                    logging.error("Couldn't retrieve image at URL: " + self.URL)
+                    continue
+
+                
+                # We at least retrieved something from the URL. Going to assume it was an image.
+
+                image = Image(snap)
+                timeFileName = time.strftime("%Y-%m-%d-%H-%M-%S") + ".jpg" # Going to filter by name for future image making so best to have a nice name.
+                try:
+                    with open(self.savePath + "/" + timeFileName, 'wb') as new_image_file: # make this less flakey for proper release
+                        new_image_file.write(image.get_file())
+                except Exception as e:
+                    logging.error("Couldn't write image to disk at " + timeFileName)
+
+
     
     def stopTimelapse(self):
         self.running = False
-        self.join()
     
+    def getImageList(self):
+        # This returns a list of escaped paths. If you're running on windows, you'll see D:\\Harrison\\ etc, 
+        if self.glob == None:
+            # not globbed yet.
+            self.glob = glob.glob(self.savePath + "/*.jpg")
+        return self.glob
 
-        
+    # FUNCTION HUGELY IMPCOMPLETE. CBF FINISHING RIGHT NOW
+    def getFilteredImageList(self, dateTimeStart, duration=1):
+        class DatedImage():
+            def __init__(self, path):
+                self.path = path
+                self.dateTime = None
 
+        imageList = self.getImageList()
+        datedImageList = []
+
+        for image in imageList:
+            dateImage = DatedImage(image)
+            datedImageList.append(DatedImage())
+        # Need to make a list of 
+        datePattern = dateTimeStart.strftime("%Y-%m-%d")
+        regex = re.compile(datePattern + r'.*')
+        return list(filter(regex.search, imageList))
 
--- webcamTimelapse.py
+++ webcamTimelapse.py
@@ -1,34 +1,27 @@
 import urllib.request
 import time
-import datetime
-import sys
+from datetime import datetime
 import os
-import glob
-import re
-import subprocess
-import copy
 from camera import Camera
 import logging
-import asyncio
 from multiprocessing import Process
-
-
+import urllib.request
+from exif import Image
+import sys
 
 ###############################################
 #                   CONFIG                    #
 ###############################################
 
-#savePath = os.path.normpath("~/timeLapse/")
-savePath = ""
+savePath = os.path.normpath(os.getcwd() + "/images/")
+logging.basicConfig(filename= savePath + "timelapse.log", encoding='utf-8', level=logging.DEBUG, format="%(asctime)s: [%(levelname)s] - %(message)s")
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 ffmpegSettings = ["-r", "30", "-f", "concat", "-safe", "0", "-vcodec", "libx264", "-crf", "25", "-pix_fmt", "yuv420p"]
-logging.basicConfig(filename= savePath + "timelapse.log", encoding='utf-8', level=logging.DEBUG)
 
 # Add cameras here
 cameraList = []
-cameraList.append(Camera("New Shed1", "192.168.0.31/snap.jpeg", 6, 8, 5))
-cameraList.append(Camera("New Shed2", "192.168.0.31/snap.jpeg", 6, 8, 5))
-cameraList.append(Camera("New Shed3", "192.168.0.31/snap.jpeg", 6, 8, 5))
-
+cameraList.append(Camera("New Shed", "http://192.168.0.31/snap.jpeg", savePath + "/NewShed", 0, 24, 5))
+cameraList.append(Camera("Harrison & Tamsin's House", "http://192.168.0.27/cgi-bin/api.cgi?cmd=Snap&channel=0&rs=as1234&user=admin&password=harrison", savePath + "/HazzTamsinHouse", 0, 24, 5))
 
 
 
@@ -36,18 +29,24 @@ cameraList.append(Camera("New Shed3", "192.168.0.31/snap.jpeg", 6, 8, 5))
 ###############################################
 #                  BUSINESS                   #
 ###############################################
-print("Staring up the time lapse system for " + str(len(cameraList)) + " cameras:")
-logging.info("Staring up the time lapse system for " + str(len(cameraList)) + " cameras:")
-proc = []
+def main():
+    logging.info("Staring up the time lapse system for " + str(len(cameraList)) + " cameras:")
+    proc = []
 
-for camera in cameraList:
-    print("  - " + camera.name)
-    logging.info("  - " + camera.name)
-    camera.start()
-
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
     for camera in cameraList:
-        camera.stopTimelapse()
+        logging.info("   " + camera.name)
+        p = Process(target = camera.startTimelapse, daemon = True)
+        proc.append(p)
+        p.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        for p in proc:
+            p.join()
+
+
+if __name__ == '__main__':
+    main()
+
